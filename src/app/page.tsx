bf0f5ebf47571d2017d2b1e61c334f072a140b7c
Saad Tahir <tahirsaad99@gmail.com>
2026-03-08 14:46:04 +0500

    disable ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
+
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const About = dynamic(() => import("@/components/About"), { ssr: false });
+const Skills = dynamic(() => import("@/components/Skills"), { ssr: false });
+const Projects = dynamic(() => import("@/components/Projects"), { ssr: false });
+const Contact = dynamic(() => import("@/components/Contact"), { ssr: false });
 
 export default function Home() {
   return (
